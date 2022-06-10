b10a2cd913191aedd02cdd5547738b57e854d545
joaoedulima <106280952+joaoelima@users.noreply.github.com>
2022-06-10 20:41:14 -0300

    .

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
+const respostaElement = document.querySelector("#resposta")
+const inputPergunta = document.querySelector ("#inputPergunta")
 const respostas = [
-    "Certeza!",
-    "Não tenho tanta certeza.",
-    "É decididamente assim.",
-    "Não conte com isso.",
-    "Sem dúvidas!",
-    "Pergunte novamente mais tarde.",
-    "Sim, definitivamente!",
-    "Minha resposta é não.",
-    "Você pode contar com isso.",
-    "Melhor não te dizer agora.",
-    "A meu ver, sim.",
-    "Minhas fontes dizem não.",
-    "Provavelmente.",
-    "Não é possível prever agora.",
-    "Perspectiva boa.",
-    "As perspectivas não são tão boas.",
-    "Sim.",
-    "Concentre-se e pergunte novamente.",
-    "Sinais apontam que sim.",
-  ]
-  
-  console.log(respostas.length)
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",
+]
+
+//clicar em fazer pergunta
+function fazerPergunta() {
+  if(inputPergunta.value == ""){
+    alert("Digite sua pergunta!")
+    return
+  }
+
+
+  // gerar numero aleatorio
+const totalResposta = respostas.length
+const numeroAleatorio = Math.floor(Math.random() * totalResposta)
+
+respostaElement.innerHTML = respostas [numeroAleatorio]
+}
